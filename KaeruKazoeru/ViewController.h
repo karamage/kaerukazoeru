48645a5fcda1de796ddfb21459c1612ba69679fc
Tomohito Ozaki <ozaki@yuroyoro.com>
2014-05-16 23:29:53 +0900

    カエルをタップしたら音を出すようにした。数のカウント処理も実装した

--- KaeruKazoeru/ViewController.h
+++ KaeruKazoeru/ViewController.h
@@ -5,9 +5,11 @@
 //  Created by Kakimoto Masaaki on 2014/05/14.
 //  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
 //
-
+#import <AVFoundation/AVFoundation.h>
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
 
 @end
+
+AVAudioPlayer *audio;
--- KaeruKazoeru/ViewController.m
+++ KaeruKazoeru/ViewController.m
@@ -7,11 +7,15 @@
 //
 
 #import "ViewController.h"
-
+#import <AVFoundation/AVFoundation.h>
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *countLabel;
 
+@property (weak, nonatomic) IBOutlet UIButton *kaeruButton;
+- (IBAction)kaeruTouch:(id)sender;
 @end
 
+static int displayCount = 0;
 @implementation ViewController
 
 - (void)viewDidLoad
@@ -26,4 +30,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+- (IBAction)kaeruTouch:(id)sender {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"1" ofType:@"mp3"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    //audio.volume = 0.1;
+    //audio.numberOfLoops = 1;
+    //[audio prepareToPlay];
+    [audio play];
+    displayCount++;
+    NSString* moji;
+    moji = [ NSString stringWithFormat : @"%d", displayCount];
+    self.countLabel.text = moji;
+}
 @end
